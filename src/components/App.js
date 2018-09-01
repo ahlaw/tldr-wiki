e5ae54935b8190b2827fe686cb63a64d83764353
Amos Law <amos.law98@gmail.com>
2018-09-01 02:00:30 -0400

    Added correct icon sizes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import CssBaseline from '@material-ui/core/CssBaseline';
 
 import HomeScreen from './HomeScreen';
 import SearchScreen from './SearchScreen';
 
 export default () => (
   <React.Fragment>
-    <CssBaseline />
     <Switch>
       <Route exact path="/" component={HomeScreen} />
       <Route path="/:id" component={SearchScreen} />
